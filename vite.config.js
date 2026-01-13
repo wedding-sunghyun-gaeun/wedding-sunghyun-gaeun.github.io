7e8630aff22c777fc6b6b0bdb74c5e90ce65c937
wedding-sunghyun-gaeun <weddingsunghyungaeun@gmail.com>
2026-01-13 23:25:36 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 import history from 'connect-history-api-fallback';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/wedding-sunghyun-gaeun.github.io/",
+    base: "/",
     plugins: [react(),
         withReactRouter(),
         tsconfigPaths(),
